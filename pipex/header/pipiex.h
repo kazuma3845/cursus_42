17939536ef438b9886a4ab73f21ea8a26a1052fd
tomuller <tomuller@student.42lausanne.ch>
2023-12-01 14:53:16 +0100

    pipex

---
+++ pipex/header/pipiex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipiex.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomuller <tomuller@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 12:51:36 by tomuller          #+#    #+#             */
+/*   Updated: 2023/12/01 12:54:25 by tomuller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+#endif
