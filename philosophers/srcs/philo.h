f498ba5a18e31cb19f457a676aa9887e37b3d453
tomuller <tomuller@student.42lausanne.ch>
2023-12-11 13:15:28 +0100

    philo

---
+++ philosophers/srcs/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomuller <tomuller@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 13:11:22 by tomuller          #+#    #+#             */
+/*   Updated: 2023/12/11 13:12:22 by tomuller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+#endif
