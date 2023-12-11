105d58bced3aa6e0aa709064c681d2919f357eec
tomuller <tomuller@student.42lausanne.ch>
2023-12-11 15:20:24 +0100

    philo

--- philosophers/srcs/main.c
+++ philosophers/srcs/main.c
@@ -6,14 +6,46 @@
 /*   By: tomuller <tomuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:10:51 by tomuller          #+#    #+#             */
-/*   Updated: 2023/12/11 13:15:11 by tomuller         ###   ########.fr       */
+/*   Updated: 2023/12/11 15:12:00 by tomuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void *algo(void *arg)
+{
+	int timer = 0;
+	int philo = 1;
+	arg = 0;
+	printf("%d %d has taken a fork", timer++, philo);
+	printf("%d %d  is eating", timer++, philo);
+	printf("%d %d  is sleeping", timer++, philo);
+	printf("%d %d  is thinking", timer++, philo);
+	printf("%d %d  died", timer, philo);
+	return (0);
+}
+
+void tread_start(void)
+{
+	int i;
+	pthread_t tid[2]; 
+
+	i = 0;
+	pthread_create(&(tid[i]), NULL, &algo, NULL);
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 4)
+	t_tab tab;
+
+	if (argc < 5 || argc > 6)
 		return (0);
+	tab.nbr_philo = atoi(argv[1]);
+	tab.time_death = atoi(argv[2]);
+	tab.time_eat = atoi(argv[3]);
+	tab.time_sleep = atoi(argv[4]);
+	if (argc == 6)
+		tab.nbr_eat = atoi(argv[5]);
+	tread_start();
+	return (0);
 }
--- philosophers/srcs/utiles.c
+++ philosophers/srcs/utiles.c
@@ -1,36 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   utiles.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tomuller <tomuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 13:11:22 by tomuller          #+#    #+#             */
-/*   Updated: 2023/12/11 14:50:34 by tomuller         ###   ########.fr       */
+/*   Created: 2023/12/11 13:30:10 by tomuller          #+#    #+#             */
+/*   Updated: 2023/12/11 13:30:22 by tomuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <fcntl.h>
-# include <limits.h>
-# include <stdarg.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <pthread.h>
+int	ft_atoi(const char *str)
 
-typedef struct s_tab
 {
-	int nbr_philo;
-	int time_eat;
-	int time_death;
-	int time_sleep;
-	int nbr_eat;
-}	t_tab;
+	int					i;
+	int					j;
+	unsigned long int	retourn;
 
-int	ft_atoi(const char *str);
-
-#endif
+	i = 0;
+	j = 1;
+	retourn = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			j *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		retourn *= 10;
+		retourn += str[i] - '0';
+		i++;
+	}
+	return (retourn * j);
+}
