e8c9bc1d16bcac99106fd9b9a928d776f1b98aef
tomuller <tomuller@student.42lausanne.ch>
2023-12-12 15:40:31 +0100

    philo

--- philosophers/srcs/main.c
+++ philosophers/srcs/main.c
@@ -6,46 +6,51 @@
 /*   By: tomuller <tomuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:10:51 by tomuller          #+#    #+#             */
-/*   Updated: 2023/12/11 15:12:00 by tomuller         ###   ########.fr       */
+/*   Updated: 2023/12/12 14:15:37 by tomuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void *algo(void *arg)
-{
-	int timer = 0;
-	int philo = 1;
-	arg = 0;
-	printf("%d %d has taken a fork", timer++, philo);
-	printf("%d %d  is eating", timer++, philo);
-	printf("%d %d  is sleeping", timer++, philo);
-	printf("%d %d  is thinking", timer++, philo);
-	printf("%d %d  died", timer, philo);
-	return (0);
-}
 
-void tread_start(void)
-{
-	int i;
-	pthread_t tid[2]; 
 
-	i = 0;
-	pthread_create(&(tid[i]), NULL, &algo, NULL);
-}
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_tab tab;
+	int				i;
+	pthread_t		tid[2];
+	pthread_mutex_t	mutex;
 
 	if (argc < 5 || argc > 6)
 		return (0);
-	tab.nbr_philo = atoi(argv[1]);
-	tab.time_death = atoi(argv[2]);
-	tab.time_eat = atoi(argv[3]);
-	tab.time_sleep = atoi(argv[4]);
-	if (argc == 6)
-		tab.nbr_eat = atoi(argv[5]);
-	tread_start();
+	argv = NULL;
+	i = 1;
+	pthread_mutex_init(&mutex, NULL);
+	pthread_create(&(tid[0]), NULL, &algo, (void *)&i);
+	i++;
+	pthread_create(&(tid[1]), NULL, &algo, (void *)&i);
+	pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
+	pthread_mutex_destroy(&mutex);
+	return (0);
+}
+
+void	*algo(void *arg)
+{
+	int	timer;
+	pthread_mutex_t	mutex;
+
+	timer = 0;
+	pthread_mutex_lock(&mutex);
+	printf("%d %d has taken a fork\n", timer, (int)arg);
+	printf("%d %d has taken a fork\n", timer, (int)arg);
+	printf("%d %d is eating\n", timer++, (int)arg);
+	// usleep(ft_atoi(argv[4]));
+	printf("%d %d is sleeping\n", timer++, (int)arg);
+	// usleep(ft_atoi(argv[5]));
+	printf("%d %d is thinking\n", timer++, (int)arg);
+	// usleep(time_for_wait_after_eat);
+	// if (argv[3] == (argv[4] + argv[5]))
+		printf("%d %d died\n", timer, (int)arg);
+	pthread_mutex_unlock(&mutex);
 	return (0);
 }
--- philosophers/srcs/philo.h
+++ philosophers/srcs/philo.h
@@ -6,7 +6,7 @@
 /*   By: tomuller <tomuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:11:22 by tomuller          #+#    #+#             */
-/*   Updated: 2023/12/11 14:50:34 by tomuller         ###   ########.fr       */
+/*   Updated: 2023/12/12 13:59:26 by tomuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,44 @@
 
 # include <fcntl.h>
 # include <limits.h>
+# include <pthread.h>
 # include <stdarg.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-# include <pthread.h>
 
-typedef struct s_tab
+typedef struct s_philo
 {
-	int nbr_philo;
-	int time_eat;
-	int time_death;
-	int time_sleep;
-	int nbr_eat;
-}	t_tab;
+	pthread_t		identifiant;
+	int				manger;
+	int				repas_mangés;
+	size_t			dernier_repas;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*mort;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}					t_philo;
 
-int	ft_atoi(const char *str);
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	repas_lock;
+	pthread_mutex_t	write_lock;
+	t_philo			*philos;
+}					t_programme;
+
+int	main(int argc, char **argv);
+int					ft_atoi(const char *str);
+void	*algo(void *arg);
 
 #endif
