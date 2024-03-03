dbcc10b54fc6f1da5609e3da5b23326f15e088fd
kazuma <tom071003@gmail.com>
2024-03-03 18:45:10 +0100

    ex00

---
+++ c++/cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void ft_print(char *str)
+{
+	int i = -1;
+
+	if (str)
+		while (str[++i])
+			std::cout << (char)toupper(str[i]);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	if (argc >= 2)
+	{
+		while (argv[++i])
+			ft_print(argv[i]);
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return 0;
+}
