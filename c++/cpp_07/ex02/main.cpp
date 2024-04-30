d4f71b831e8a6ab5ce5ee32763e16c974b71189a
kazuma <tom071003@gmail.com>
2024-04-30 11:10:10 +0200

    cpp07

--- c++/cpp_07/ex02/main.cpp
+++ c++/cpp_07/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include "Array.hpp"
 
 #define MAX_VAL 750
@@ -15,8 +16,44 @@ int main(int, char**)
     }
     //SCOPE
     {
-        Array<int> tmp = numbers;
+        Array<int> tmp;
+        try
+        {
+            for (int i = 0; i < MAX_VAL + 1; i++)
+                std::cout << "tmp[" << i << "]:\t" << tmp[i] << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\t';
+            std::cerr << "You tried to access past the last element of the array" << std::endl;
+        }
+        tmp = numbers;
         Array<int> test(tmp);
+
+        try
+        {
+            for (int i = 0; i < MAX_VAL + 1; i++)
+        {
+            if (tmp[i] != test[i])
+            {
+                std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+        }
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\t';
+            std::cerr << "You tried to access past the last element of the array" << std::endl;
+        }
+
+        std::cout << std::endl << "test[1]:\t" << test[1] << std::endl;
+        std::cout << "tmp[1]:\t\t" << tmp[1] << std::endl << std::endl;
+
+        test[1] = 123456789;
+
+        std::cout << "test[1]:\t" << test[1] << std::endl;
+        std::cout << "tmp[1]:\t\t" << tmp[1] << std::endl << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -33,7 +70,8 @@ int main(int, char**)
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
+        // std::cout << "index was -2" << std::endl;
     }
     try
     {
@@ -41,13 +79,18 @@ int main(int, char**)
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
+        // std::cout << "tried to access past the last element of the array" << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
     }
-    delete [] mirror;//
+    delete [] mirror;
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     std::cout << numbers[i] << std::endl;
+    // }
     return 0;
 }
